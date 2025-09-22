2b7a8083dc65c9efa97e2630f1af7995b0b87bec
CodeVANIE <kungfuninja302@gmail.com>
2025-09-22 12:28:49 +0800

    Fix overscrolling white background overlapping in mobile. 1 background only for content. Fix scrolling sensitivity in mobile list manager.

--- src/layouts/ContentWrapper.jsx
+++ src/layouts/ContentWrapper.jsx
@@ -1,12 +1,8 @@
-import { useLocation } from "react-router-dom";
 
 export default function ContentWrapper({ children }) {
-    const { pathname } = useLocation();
     
     return (
-        <main className={`grow bg-black/60 bg-blend-multiply bg-center bg-no-repeat bg-cover bg-fixed  
-        ${/^\/modify/.test(pathname) ? "bg-smooth-brown" : "bg-rough-brown"}`}>
-
+        <main className="grow">
             {children}
         </main>
     )
--- src/pages/Modify/Sheets/Sheet.jsx
+++ src/pages/Modify/Sheets/Sheet.jsx
@@ -2,7 +2,7 @@
 export default function Sheet({ title, onSelect, isSelected, children }) {
     
     return (
-        <div className={`shrink-0 basis-full rounded-3xl snap-center max-w-lg overflow-hidden border-x-5 border-amber-700/75
+        <div className={`shrink-0 basis-full rounded-3xl snap-center snap-always max-w-lg overflow-hidden border-x-5 border-amber-700/75
              ${isSelected ? "border-dotted" : "border-none max-md:pointer-events-none"}`}>
             <h1 className={`py-2 text-center text-2xl font-bold text-yellow-950 cursor-pointer 
                 ${isSelected ? "bg-ptlbrown-100" : "bg-ptlbrown-100/60 hover:bg-ptlbrown-100/70"}`} onClick={onSelect}>
