27c3a3fce6b5a38181fba76b1579f533f908cd64
CodeVANIE <kungfuninja302@gmail.com>
2025-09-26 22:29:03 +0800

    Fix Home Content Wrapper

--- src/layouts/HomeContentWrapper.jsx
+++ src/layouts/HomeContentWrapper.jsx
@@ -2,7 +2,7 @@
 export default function HomeContentWrapper({ children }) {
     
     return (
-        <div className="grow max-w-4xl p-3 mx-auto relative">
+        <div className="grow max-w-4xl p-3 mx-auto relative min-w-0">
             {children}
         </div>
     )
